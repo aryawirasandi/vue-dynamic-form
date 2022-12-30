6837761d126fc4982933796cec62f8f7345ba58d
aryawirasandi <aryawirasandi7153@gmail.com>
2022-12-30 21:03:30 +0800

    set skip for unit test

--- src/__test__/forms.spec.ts
+++ src/__test__/forms.spec.ts
@@ -1,4 +1,17 @@
 import { describe, expect, it } from "vitest";
 import { mount } from "@vue/test-utils";
-import Forms from "../components/forms.vue";
+import Forms from "@/components/forms.vue";
+const db = require("../api/db.json");
+describe("Form Component", () => {
+    it.skip("the component should return input type text", async () => {
+        const wrapper = mount(Forms, {
+            props: {
+                forms : db
+            }
+        });
+        const input = wrapper.find("input[type='text']");
+        await wrapper.find("input[type='text']").setValue("Hello World")
 
+        expect(input.element).toContain("Hello World");
+    })
+})
